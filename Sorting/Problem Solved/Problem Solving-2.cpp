b8a2420102db53cfcfdfdbd9c4b7b91c81f5520f
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-02-21 22:06:55 +0600

    Add files via upload

---
+++ Sorting/Problem Solved/Problem Solving-2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter the size of the array : ";
+    int n;
+    cin>>n;
+
+    cout<<"Enter Elements for the array : ";
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int swaps = 0;
+    int comparisons = 0;
+
+    // Syntex For Insertion Sort
+    for(int i=1; i<n; i++)
+    {
+        int temp = arr[i];
+        int j = i-1;
+
+        while(j >= 0 && arr[j] > temp)
+        {
+            arr[j+1] = arr[j];
+            j--;
+            swaps++;
+            comparisons++;
+        }
+        arr[j+1] = temp;
+        swaps++;
+    }
+
+    cout<<"Sorted Array : ";
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<"\nNumber of swaps: "<<swaps<<endl;
+    cout<<"Number of comparisons: "<<comparisons<<endl;
+
+    getch();
+}
+
