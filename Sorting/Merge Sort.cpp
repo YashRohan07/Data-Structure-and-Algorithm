17aafdc21bc97edaa97e86b91b27776daedbd978
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-02-21 21:45:06 +0600

    Add files via upload

---
+++ Sorting/Merge Sort.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+using namespace std;
+
+// Conquer Part
+void merge(int arr[], int left, int mid, int right)
+{
+    int i = left, j = mid + 1, k = 0;
+    int temp[right - left + 1];
+
+    while (i <= mid && j <= right)
+    {
+        if(arr[i] <= arr[j]) {
+            temp[k] = arr[i];
+            i++;
+        }
+        else{
+            temp[k] = arr[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= right)
+    {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+     for (int i = left; i <= right; i++)
+    {
+        arr[i] = temp[i-left];
+    }
+
+    /*
+       The loop you are asking about copies the merged elements from the temporary array
+       "temp" back into the original array "arr". The index "left" is the starting index of the left
+       subarray, and the index "i" represents the index of the current element being copied from
+       "temp" back into "arr". Therefore, "arr[left + i]" is the correct index in "arr"
+       to copy the current element from "temp".
+    */
+
+}
+
+// Divide Part
+void mergeSort(int arr[], int left, int right)
+{
+    if (left >= right)
+    {
+        return;     //When the array has one element
+    }
+
+    int mid = (left + right) / 2;
+
+    mergeSort(arr, left, mid);       // For 1st step
+    mergeSort(arr, mid + 1, right);  // For 2nd step
+
+    merge(arr, left, mid, right);    // For Sorting the array
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+
+
+/*
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+*/
